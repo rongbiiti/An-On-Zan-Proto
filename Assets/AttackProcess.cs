956666663761ae57f5b106c4d19f44edbb86b1b4
rongbiiti <11807078@stu.kbc.ac.jp>
2020-09-12 19:22:01 +0900

    最初のコミッツ

---
+++ Assets/AttackProcess.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackProcess : MonoBehaviour
+{
+
+    [SerializeField]
+    private BoxCollider weaponCollider;
+
+    [SerializeField]
+    private GameObject effect;
+
+    private void Start()
+    {
+        effect.SetActive(false);
+    }
+
+    void AttackStart()
+    {
+        weaponCollider.enabled = true;
+        Debug.Log("攻撃判定ON");
+        effect.SetActive(true);
+    }
+
+    void AttackEnd()
+    {
+        weaponCollider.enabled = false;
+        Debug.Log("攻撃判定OFF");
+        effect.SetActive(false);
+    }
+}
---
+++ Assets/HitProcess.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class HitProcess : MonoBehaviour
+{
+
+    [SerializeField]
+    private AudioClip deathVoiceClip;
+
+    [SerializeField]
+    private AudioClip hitClip;
+
+    [SerializeField]
+    private AudioMixerGroup audioMixerGroup;
+
+    [SerializeField]
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlaydeathVoice()
+    {
+        audioSource.PlayOneShot(deathVoiceClip);
+    }
+
+    public void PlayHit()
+    {
+        audioSource.PlayOneShot(hitClip);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Enemy")) {
+            PlaydeathVoice();
+            PlayHit();
+            other.gameObject.SetActive(false);
+        }
+    }
+}
