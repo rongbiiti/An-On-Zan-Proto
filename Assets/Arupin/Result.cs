6c2a0330acc7c1825c588a72c43646e9e5767a8c
ryugo_1220 <11807130@stu.kbc.ac.jp>
2020-11-11 12:51:16 +0900

    決着時アニメーション後に、再戦選択画面が出てくるようにした。

---
+++ Assets/Arupin/Result.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Result : MonoBehaviour
+{
+    public GameObject result;
+
+    public void ResultEneble()
+    {
+        result.SetActive(true);
+    }
+}
