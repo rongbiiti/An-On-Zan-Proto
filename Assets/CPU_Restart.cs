d8ed956eed002af508278203bb17ab8ed726a537
ki1180 <11806117@stu.kbc.ac.jp>
2020-11-12 15:15:59 +0900

    ・CPU 戦でリザルト画面の遷移を付けました。(ネットワーク未実装) 空条仗世文

---
+++ Assets/CPU_Restart.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// 1.UIシステムを使うときに必要なライブラリ
+using UnityEngine.UI;
+// 2.Scene関係の処理を行うときに必要なライブラリ
+using UnityEngine.SceneManagement;
+
+public class CPU_Restart : MonoBehaviour
+{
+    public void SceneReload()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+}
