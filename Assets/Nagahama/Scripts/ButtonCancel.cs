eb9d6a7328752f7ced4c74247daf70dde2ad7669
hayato nagahama <11807078@stu.kbc.ac.jp>
2020-11-18 16:45:34 +0900

    UIのキャンセル挙動追加

---
+++ Assets/Nagahama/Scripts/ButtonCancel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonCancel : MonoBehaviour
+{
+    [SerializeField] private Selectable _selectable;
+
+    public void FocusButton()
+    {
+        _selectable.Select();
+    }
+}
