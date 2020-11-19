193203b007cb5b7bdcca38d13f0616f7efbdf43a
ryugo_1220 <11807130@stu.kbc.ac.jp>
2020-11-19 12:46:01 +0900

    ネットワーク対戦のリザルト画面に再戦処理実装

---
+++ Assets/Arupin/Online_Restart.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+// 1.UIシステムを使うときに必要なライブラリ
+using UnityEngine.UI;
+// 2.Scene関係の処理を行うときに必要なライブラリ
+using UnityEngine.SceneManagement;
+
+public class Online_Restart : MonoBehaviour
+{
+    public void SceneReload()
+    {
+        RoomManager.Instance.isCreatead = false;
+        PhotonNetwork.LoadLevel(2);
+        PhotonNetwork.CurrentRoom.IsOpen = false;
+        PhotonNetwork.CurrentRoom.IsVisible = false;
+    }
+
+    public void LeaveRoom()
+    {
+        PhotonNetwork.LeaveRoom();
+    }
+
+}
