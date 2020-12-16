2d63caea3a923e57e681c38de5949429d7dd2191
ryugo_1220 <11807130@stu.kbc.ac.jp>
2020-12-17 00:33:10 +0900

    リプレイをボタン押して視点切り替えする方に変更

---
+++ Assets/Arupin/ReplayCamera_Transform.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReplayCamera_Transform : MonoBehaviour
+{
+    Vector3 pos;
+    Quaternion rot;
+    private int cameracount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pos = gameObject.transform.position;
+        rot = gameObject.transform.rotation;
+        cameracount = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            if(cameracount < 3)
+            {
+                cameracount++;
+            }else{
+                cameracount = 0;
+            }
+            ChangeCameraPosiyion();
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            if (cameracount > 0)
+            {
+                cameracount--;
+            }
+            else{
+                cameracount = 3;
+            }
+            ChangeCameraPosiyion();
+        } 
+    }
+
+    void ChangeCameraPosiyion()
+    {
+        switch (cameracount)
+        {
+            case 0:
+                gameObject.transform.position = new Vector3(6, 9.98f, -6);
+                gameObject.transform.rotation = Quaternion.Euler(35, -45, 0);
+                break;
+            case 1:
+                gameObject.transform.position = new Vector3(6, 9.98f, 6);
+                gameObject.transform.rotation = Quaternion.Euler(35, -135, 0);
+                break;
+            case 2:
+                gameObject.transform.position = new Vector3(-6, 9.98f, 6);
+                gameObject.transform.rotation = Quaternion.Euler(35, 135, 0);
+                break;
+            case 3:
+                gameObject.transform.position = new Vector3(-6, 9.98f, -6);
+                gameObject.transform.rotation = Quaternion.Euler(35, 45, 0);
+                break;
+        }
+    }
+}
